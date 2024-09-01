a84634a728aa7ee945da6374871ea0a584bc482b
Jakob Evangelista <jakobevangelista@gmail.com>
2024-09-01 13:03:09 -0700

    middleware checks env for routes

--- apps/auth/src/middleware.ts
+++ apps/auth/src/middleware.ts
@@ -1,6 +1,26 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, currentUser } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware(async (auth, request) => {
+  const user = auth();
+  //   const user = await currentUser();
+
+  if (user.userId !== null) {
+    // Check user attributes and redirect
+    // if (user.samlAccounts.length > 0) {
+    const res = await fetch(`https://api.clerk.com/v1/users/${user.userId}`, {
+      headers: {
+        Authorization: `Bearer ${process.env.CLERK_SECRET_KEY}`,
+      },
+    });
+    const data = await res.json();
+    // console.log(data.saml_accounts.length);
+    if (data.saml_accounts.length > 0) {
+      return Response.redirect(new URL(`${process.env.BUSINESS_URL}`));
+    } else {
+      return Response.redirect(new URL(`${process.env.CONSUMER_URL}`));
+    }
+  }
+});
 
 export const config = {
   matcher: [
